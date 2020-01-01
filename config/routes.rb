ffbe2cecfd63f8102fa05662871771014b392469
kykt35 <kykt35@gmail.com>
2020-01-01 16:40:50 +0900

    Edit routex

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   use_doorkeeper
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: :index
+
+  post '/token', to: 'token#create'
 end
